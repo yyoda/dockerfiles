91e5973b774e872936aadb9a1258b1068db7d6ec
unknown <>
2016-11-01 18:22:49 +0900

    refoctoring

--- iis_net45/setup.ps1
+++ iis_net45/setup.ps1
@@ -2,10 +2,14 @@ mkdir C:\IIS\FailedReqLogFiles;
 mkdir C:\IIS\IISLogFiles;
 mkdir C:\IIS\wwwroot;
 
-New-WebAppPool -Name example;
-New-WebSite -Name example -PhysicalPath C:\IIS\wwwroot -Port 80 -ApplicationPool example;
+New-WebAppPool -Name default;
+New-WebSite -Name default -PhysicalPath C:\IIS\wwwroot -Port 80 -ApplicationPool default;
 
-$webSite = Get-Item IIS:\Sites\example;
+$webSite = Get-Item IIS:\Sites\default;
 $webSite.logFile.period = "Hourly";
 $webSite.logFile.directory = "C:\IIS\IISLogFiles";
 $webSite | Set-Item;
+
+# Web発行などが必要な場合はユーザを作成しておく必要がある
+# net user /add develop somepwd
+# net localgroup administrators develop /add
